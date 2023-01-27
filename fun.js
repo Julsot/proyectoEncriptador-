0023baca8174326464efb2bcbc7f685a7b28b708
Julsot <90521985+Julsot@users.noreply.github.com>
2023-01-27 17:58:06 -0300

    Delete fun.js

--- fun.js
+++
@@ -1,9 +0,0 @@
-function encriptar(){
-    var carga = document.getElementById("encriptado").ariaValueMax.toLowerCase()
-
-    var encriptado = carga.replace(/e/img, "enter")
-    var encriptado = carga.replace(/i/img, "imes")
-    var encriptado = carga.replace(/a/img, "ai")
-    var encriptado = carga.replace(/o/img, "ober")
-    var encriptado = carga.replace(/u/img, "ufat")
-}
